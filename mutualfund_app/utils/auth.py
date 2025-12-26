74c769c9be25f566fa7504007906a8bbf306484b
sumansourabhcsc <sumansourabhcsc@gmail.com>
2025-12-26 22:01:35 +0100

    Update auth.py

--- mutualfund_app/utils/auth.py
+++ mutualfund_app/utils/auth.py
@@ -1,1 +1,20 @@
+import streamlit as st
 
+def global_auth():
+    if "auth" not in st.session_state:
+        st.session_state.auth = False
+
+    if st.session_state.auth:
+        return True
+
+    st.title("🔐 Secure Access")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Login"):
+        if password == st.secrets["LOGIN_PASSWORD"]:
+            st.session_state.auth = True
+            st.rerun()
+        else:
+            st.error("Incorrect password")
+
+    st.stop()
