ce606182d084e0a64c0c8dffdb59f7d5d474275f
sumansourabhcsc <sumansourabhcsc@gmail.com>
2025-12-26 22:04:17 +0100

    Update formatting.py

--- mutualfund_app/utils/formatting.py
+++ mutualfund_app/utils/formatting.py
@@ -1,1 +1,9 @@
-
+def format_indian(n):
+    n = float(n)
+    if abs(n) >= 1_00_00_000:
+        return f"{n/1_00_00_000:.2f} Cr"
+    elif abs(n) >= 1_00_000:
+        return f"{n/1_00_000:.2f} L"
+    elif abs(n) >= 1_000:
+        return f"{n/1_000:.2f} K"
+    return f"{n:.0f}"
